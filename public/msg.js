703cc2aa56fa9f2d5460d9b934ffbf85f5a665a3
Umesha Rukshan Silva <umesharukshan2000@gmail.com>
2024-06-20 16:23:07 +0530

    h

--- public/msg.js
+++ public/msg.js
@@ -1,7 +1,14 @@
 function showPopup() {
+
+    const totalElement = document.getElementById("total");
+    const modalTotalElement = document.getElementById("modalTotal");
+    modalTotalElement.textContent = totalElement.textContent;
+  
+
     document.getElementById('popupModal').style.display = 'block';
   }
   
   function closePopup() {
+
     document.getElementById('popupModal').style.display = 'none';
   }
